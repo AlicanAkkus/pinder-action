717b48fe973de10a1d4f3c3cc7e941aba0ae3a57
Alican Akkus <alican.akkus94@gmail.com>
2019-12-10 22:36:29 +0300

    adds ls for debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+current_dir=$(ls -a)
 search_result=$(grep -r -i -n "password[=:]" .)
+
+echo "Current dir; \n $current_dir"
 
 if [[ $search_result ]]; then
     echo "Sensitive data found! Check it out! \n $search_result"
