c5f8200470fcd4eb7caff325ec0f3ce00e9d26bb
Alican Akkus <alican.akkus94@gmail.com>
2019-12-10 22:57:53 +0300

    use github workspace as volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-search_result=$(grep -r -i -n "password[=:]" $GITHUB_WORKSPACE)
+search_result=$(grep -r -i -n "password[=:]" /github/workspace)
 
 if [[ $search_result ]]; then
     echo "Sensitive data found! Check it out! \n $search_result"
