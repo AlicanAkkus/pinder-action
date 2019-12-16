3beb763404ba8d507bf7545b5ca95f3eb6f5eaeb
Alican Akkus <alican.akkus94@gmail.com>
2019-12-16 09:03:53 +0300

    increase verison

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-echo "pwd -> $(pwd)"
-echo "list /github/workflow -> $(ls -a /github/workflow)"
-echo "list /github/workspace -> $(ls -a /github/workspace)"
-echo "list /github/home -> $(ls -a /github/home)"
-search_result=$(grep -r -i -n "password[=:]" /github/workflow)
+search_result=$(grep -r -i -n "password[=:]" $GITHUB_WORKSPACE)
 
 if [[ $search_result ]]; then
     echo "Sensitive data found! Check it out! \n $search_result"
